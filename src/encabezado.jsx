fbf32cfb515706ed4d787af073f898c6d9de2514
AngieMorenoOrtega <guaiagaleria@gmail.com>
2023-12-03 21:04:04 -0500

    logica de encabezado completa

--- src/encabezado.jsx
+++ src/encabezado.jsx
@@ -5,7 +5,7 @@ const Encabezado = () => {
   const [empresa, setEmpresa] = useState({});
   const [clientes, setClientes] = useState([]);
   const [fechaActual, setFechaActual] = useState("");
-
+  const [selectedClient, setSelectedClient] = useState(null);
   useEffect(() => {
     // Obtener datos de la empresa
     axios
@@ -13,10 +13,13 @@ const Encabezado = () => {
       .then((response) => setEmpresa(response.data[0])); // Asumo que solo necesitas un registro
 
     // Obtener datos de los clientes
-    axios
-      .get("http://localhost:3001/clientes")
-      .then((response) => setClientes(response.data));
-
+    axios.get("http://localhost:3001/clientes").then((response) => {
+      setClientes(response.data);
+      // Seleccionar el primer cliente por defecto
+      if (response.data.length > 0) {
+        setSelectedClient(response.data[0]);
+      }
+    });
     // Obtener fecha actual
     const fecha = new Date();
     setFechaActual(
@@ -25,28 +28,30 @@ const Encabezado = () => {
   }, []); // Se ejecuta solo una vez al cargar el componente
 
   console.log(clientes);
-  console.log(empresa);
+  console.log(selectedClient);
   return (
     <div>
       <h1>Encabezado</h1>
       <div>
         <h2>Datos de la Empresa</h2>
-        <p>NIT: {empresa.NitEmpresa}</p>
-        <p>Dirección: {empresa.Direccion}</p>
-        <p>Celular: {empresa.Celular}</p>
-        <p>Locación: {empresa.Locacion}</p>
-        <p>Empresa: {empresa.Empresa}</p>
-        <img src={empresa.Imagen} alt="Logo de la empresa" />
+        <p>Nombre: {empresa.nombre}</p>
+        <p>NIT: {empresa.nit}</p>
+        <p>Dirección: {empresa.direccion}</p>
+        <p>Celular: {empresa.telefono}</p>
+        <p>Email {empresa.email}</p>
+        <img src={empresa.imagen} alt="Defrescura" />
       </div>
       <div>
         <h2>Clientes</h2>
-        <ul>
-          {clientes.map((cliente) => (
-            <li key={cliente.Cliente}>
-              {cliente.Cliente} - {cliente.Sucursal}
+        {/* <ul>
+          {selectedClient.map((cliente) => (
+            <li key={cliente.nombre}>
+              <li>{cliente.telefono}</li>
+              <li>{cliente.correo}</li>
+              {cliente.direccion} - {cliente.sucursal}
             </li>
           ))}
-        </ul>
+        </ul> */}
       </div>
       <div>
         <h2>Fecha Actual</h2>
